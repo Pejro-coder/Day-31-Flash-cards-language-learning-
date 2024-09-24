6229acaf704730fbf433040c6e1fd6e87df2437c
Pejro-coder <peter.stepanic@hotmail.com>
2024-09-24 16:11:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from tkinter import *
 import random
 import pandas
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/french_words.csv")
+print(data)
+to_learn = data.to_dict(orient="records")
+print(to_learn)
+
+
+# list_of_dicts = [{row['French']: row['English']} for _, row in data.iterrows()]
+# list_of_lists = [[row['French'], row['English']] for _, row in data.iterrows()]
+# print(list_of_dicts)
+# print(list_of_lists)
+
 
 # ----------------------------------WORD RESET LOGIC----------------------------------
-def chose_new_word():
-    foreign_word = random.choice(data["French"])
-    canvas.create_image(400, 263, image=card_image)
-    canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 26, "italic"))
-    text = canvas.create_text(400, 260, text=foreign_word, fill="black", font=("Ariel", 32, "bold"))
+def next_card():
+    current_card = random.choice(to_learn)
+    current_word = current_card["French"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_text, text=current_word)
+
+    window.after(3000, flip_card, current_card)
+
+
+def flip_card(current_card):
+    current_word = current_card["English"]
+    canvas.itemconfig(canvas_image, image=back_image)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_text, text=current_word, fill="white")
 
 
 # --------------------------------------UI SETUP--------------------------------------
@@ -19,27 +40,24 @@ window.title("Flash cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Card
-data = pandas.read_csv("data/french_words.csv")
-data.to_dict()
-print(data)
-print(data["French"].to_list())
-foreign_word = random.choice(data["French"])
-
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-card_image = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=card_image)
-canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 26, "italic"))
-canvas.create_text(400, 260, text=foreign_word, fill="black", font=("Ariel", 32, "bold"))
+back_image = PhotoImage(file="images/card_back.png")
+front_image = PhotoImage(file="images/card_front.png")
+canvas_image = canvas.create_image(400, 263, image=front_image)
+card_title = canvas.create_text(400, 150, text="BACK", fill="black", font=("Ariel", 26, "italic"))
+card_text = canvas.create_text(400, 260, text="word", fill="black", font=("Ariel", 32, "bold"))
 canvas.grid_configure(row=0, column=0, columnspan=2, pady=10)
 
 # Confirm button
 checkmark_image = PhotoImage(file="images/right.png")
-button_right = Button(image=checkmark_image, highlightthickness=0, command=chose_new_word)
+button_right = Button(image=checkmark_image, highlightthickness=0, command=next_card)
 button_right.grid_configure(row=1, column=0)
 
 # Wrong button
 cross_image = PhotoImage(file="images/wrong.png")
-button_left = Button(image=cross_image, highlightthickness=0, command=chose_new_word)
+button_left = Button(image=cross_image, highlightthickness=0, command=next_card)
 button_left.grid_configure(row=1, column=1)
+
+next_card()
 
 mainloop()
